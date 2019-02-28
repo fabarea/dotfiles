018802fb6179fd147c26558f9dc2a064ef83f5bc
Fabien Udriot <fabien@omic.ch>
2019-02-28 15:11:12 +0100

    Comment a bunch of settings for macOS environment

--- src/os/install/macos/browsers.sh
+++ src/os/install/macos/browsers.sh
@@ -13,22 +13,22 @@ print_in_purple "\n   Browsers\n"
 printf "\n"
 
 brew_install "Chrome" "google-chrome" "caskroom/cask" "cask"
-brew_install "Chrome Canary" "google-chrome-canary" "caskroom/versions" "cask"
-brew_install "Chromium" "chromium" "caskroom/cask" "cask"
+#brew_install "Chrome Canary" "google-chrome-canary" "caskroom/versions" "cask"
+#brew_install "Chromium" "chromium" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 printf "\n"
 
 brew_install "Firefox" "firefox" "caskroom/cask" "cask"
-brew_install "Firefox Developer" "firefoxdeveloperedition" "caskroom/versions" "cask"
-brew_install "Firefox Nightly" "firefoxnightly" "caskroom/versions" "cask"
+#brew_install "Firefox Developer" "firefoxdeveloperedition" "caskroom/versions" "cask"
+#brew_install "Firefox Nightly" "firefoxnightly" "caskroom/versions" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 printf "\n"
 
-brew_install "Flash" "flash-npapi" "caskroom/cask" "cask"
+#brew_install "Flash" "flash-npapi" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -37,5 +37,5 @@ brew_install "Flash" "flash-npapi" "caskroom/cask" "cask"
 
 if is_supported_version "$(get_os_version)" "10.11.4"; then
     printf "\n"
-    brew_install "Safari Technology Preview" "safari-technology-preview" "caskroom/versions" "cask"
+#    brew_install "Safari Technology Preview" "safari-technology-preview" "caskroom/versions" "cask"
 fi
