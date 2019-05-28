8f982a1d8f1368a15a5ee2dcb1df249dbacdcb50
Fabien Udriot <fabien@omic.ch>
2019-05-28 22:12:54 +0200

    Better integration with brew on Ubuntu

--- src/os/install/ubuntu/tools.sh
+++ src/os/install/ubuntu/tools.sh
@@ -6,16 +6,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-print_in_purple "\n   Dev Tools\n\n"
+print_in_purple "\n   Development Tools\n\n"
 
 install_package "Git" "git"
 install_package "Tig" "tig"
 install_package "ack" "ack"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-#print_in_purple "\n   tmux\n\n"
-
 #install_package "tmux" "tmux"
 
 
