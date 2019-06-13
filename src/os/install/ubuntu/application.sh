54601945533e2bd031e806d54091795d0dd40ff2
Fabien Udriot <fabien@omic.ch>
2019-06-13 20:12:01 +0200

    Beautify code

--- src/os/install/ubuntu/application.sh
+++ src/os/install/ubuntu/application.sh
@@ -8,10 +8,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Applications\n\n"
 
-install_package "VPN IPSec / L2TP"  "network-manager-l2tp"
-install_package "VPN IPSec / L2TP Gnome Preference"  "network-manager-l2tp-gnome"
-install_package "Transmission"  "transmission"
-install_package "VLC"           "vlc"
+install_package "VPN IPSec / L2TP"          "network-manager-l2tp"
+install_package "VPN Gnome Preference"      "network-manager-l2tp-gnome"
+install_package "GNOME Shell integration"   "chrome-gnome-shell"
+install_package "Transmission"              "transmission"
+install_package "VLC"                       "vlc"
+
 snap_install    "VSCode"        "code"          "--classic"
 snap_install    "Sublime Text"  "sublime-text"  "--classic"
 snap_install    "PHPStorm"      "phpstorm"      "--classic"
