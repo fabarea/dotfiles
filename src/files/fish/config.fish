0b3ea0eeb1017ae33ca5390f5d8d3d7fef74f6b7
Fabien Udriot <fabien@omic.ch>
2019-06-14 10:51:24 +0200

    Add alias for ddev

--- src/files/fish/config.fish
+++ src/files/fish/config.fish
@@ -4,10 +4,14 @@ alias ..="cd ../";
 alias ...="cd ../..";
 alias ....="cd ../../..";
 alias cdp="cd ~/Projects";
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 alias j='z';
 alias jj='zz';
+alias d='ddev';
+alias dc='docker-compose';
 alias dep='php ./vendor/bin/dep'
-export GANDI_LIVEDNS_KEY="mrGbV8YSZiHCFOEzMKanPAdg"
 
 #set -gx PATH $HOME/.cargo/bin $HOME/.composer/vendor/bin $PATH
 #status --is-interactive; and source (pyenv init -|psub)
