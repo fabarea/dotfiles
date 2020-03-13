c5d68a6f5734958c25d6ba2033a2405fdc88a62c
Fabien Udriot <fabien@omic.ch>
2020-03-13 09:47:06 +0100

    Add preferences script

---
+++ preferences.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Just a shorthand version to setup the preferences...
+~/.dotfiles/src/os/preferences.sh
---
+++ src/os/preferences.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+declare dotfilesDirectory="$HOME/.dotfiles"
+declare skipQuestions=true
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+    # Ensure that the following actions
+    # are made relative to this file's path.
+
+    cd "$(dirname "${BASH_SOURCE[0]}")" \
+        || exit 1
+
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    skip_questions "$@" \
+        && skipQuestions=true
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    ask_for_sudo
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    ./preferences/main.sh
+}
+
+main "$@"
